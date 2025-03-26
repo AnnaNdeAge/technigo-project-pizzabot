9827cff30e9a7de0f48d3901b98f5d55c478848c
AnnaNdeAge <annandeage@gmail.com>
2025-03-26 12:37:01 +0100

    Finished first two sections

--- code/script.js
+++ code/script.js
@@ -4,10 +4,26 @@
 // Your code goes here
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+);
+
+let person = prompt("Please enter your name:", "no name");
+
+if (person !== null) {
+  alert('Hello, ' + person + ', welcome to our Foodbot!')
+}
 
 // Step 2 - Food choice
-// Your code goes here
+let foodChoice = parseInt(prompt("What type of food would you like to eat? \nEnter a number: \n1 - Pizza \n2 - Pasta \n3 - Salad"))
+if (foodChoice === 1) {
+  alert("You have choosen Pizza!")
+} else if (foodChoice === 2) {
+  alert("You have choosen Pasta")
+} else if (foodChoice === 3) {
+  alert("You have choosen Salad")
+} else {
+  alert("Please enter a valid number!")
+}
+
 
 // Step 3 - Subtype choice
 // Your code goes here
